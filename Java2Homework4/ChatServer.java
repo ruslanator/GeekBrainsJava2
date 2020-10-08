bc70b45c42ce5ea3ff54088d97b0158755c5a704
ruslanator <latyshevruslan@ya.ru>
2020-10-08 23:10:55 +0300

    HW4

---
+++ Java2Homework4/ChatServer.java
@@ -0,0 +1,11 @@
+package Java2Homework4;
+
+public class ChatServer {
+    public void start(int port) {
+        System.out.println("Server started at: " + port);
+    }
+
+    public void stop() {
+        System.out.println("Server stopped");
+    }
+}
